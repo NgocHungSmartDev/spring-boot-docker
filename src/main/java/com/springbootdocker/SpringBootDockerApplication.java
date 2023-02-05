7c77c34594a1d66dd81cfa99e21dc43cf79d47c7
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-05 18:40:17 +0700

    add config DB

--- src/main/java/com/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/springbootdocker/SpringBootDockerApplication.java
@@ -1,13 +1,31 @@
 package com.springbootdocker;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("hello")
+    public String helloDocker() {
+        Integer idleConnection = new HikariDataSourcePoolMetadata((HikariDataSource) dataSource).getIdle();
+        return "Hello Docker! Idle connection to database is " + idleConnection;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 
 }
